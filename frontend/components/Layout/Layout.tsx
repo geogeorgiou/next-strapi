67fc275c22bc05cecbe831e0651582a9412518f7
geogeorgiou <georgiogeo27@gmail.com>
2022-09-25 21:03:31 +0300

    adding Layout changes, Nav component

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,16 +1,20 @@
 import styles from "../../styles/Layout.module.css"
 import React from "react";
+import {Nav} from "../Nav";
+
+export const Layout = ({children}: LayoutProps) => {
+    return (
+        <>
+            <Nav/>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+            </div>
+        </>
+    );
+};
 
 type LayoutProps = {
     children: React.ReactNode
 }
-
-export const Layout = ({children}: LayoutProps) => {
-    return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                {children}
-            </main>
-        </div>
-    );
-};
